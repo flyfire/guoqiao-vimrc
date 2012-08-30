81a017910c654122e197cf8bc0fa0f39586380bc
guoqiao <guoqiao@gmail.com>
2012-08-30 17:09:03 +0800

    improve install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
+
+# Absolute path to this script. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f "${PWD}/${BASH_SOURCE}")
+
+# Absolute path this script is in. /home/user/bin
+TOP_PATH=`dirname "$SCRIPT"`
+
 echo rm old files...
 rm ~/.vimrc
 rm -rf ~/.vim
+
 echo link new files...
-ln -s ~/git/guoqiao-vimrc/_vimrc ~/.vimrc
-ln -s ~/git/guoqiao-vimrc/vimfiles ~/.vim
+ln -s $TOP_PATH/_vimrc ~/.vimrc
+ln -s $TOP_PATH/vimfiles ~/.vim
+
 echo done!
