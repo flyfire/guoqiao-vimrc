942ff75858f0aeb3f06be716dfd04eec81211936
guoqiao <guoqiao@gmail.com>
2012-12-22 16:12:49 +0800

    rename to sync

--- vimfiles/helper.py
+++ vimfiles/helper.py
@@ -12,9 +12,10 @@ GIT_REPOS = (
         'https://github.com/mattn/zencoding-vim',
         'https://github.com/kevinw/pyflakes-vim.git',
         'https://github.com/vim-scripts/NERD_Tree-and-ack.git',
+        'https://github.com/vim-scripts/python-imports.vim.git',
         )
 
-def fetch():
+def sync():
     for r in GIT_REPOS:
         name = r.strip('/').split('/')[-1]
         path = os.path.join(BUNDLE,name)
@@ -31,14 +32,14 @@ def fetch():
 
 def main():
     parser = OptionParser()
-    parser.add_option("-f", "--fetch",
-                      action="store_true", dest="fetch", default=False,
-                      help="fetch all repos")
+    parser.add_option("-s", "--sync",
+                      action="store_true", dest="sync", default=False,
+                      help="sync all repos")
 
     (options, args) = parser.parse_args()
 
-    if options.fetch:
-        fetch()
+    if options.sync:
+        sync()
     else:
         parser.print_help()
 
